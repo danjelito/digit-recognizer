bc09e00948b5a5854238a4ad9583e40a9de3d3ef
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-10 20:07:29 +0700

    Debug image conversion, have not work.

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
-import matplotlib.pyplot as plt
 from skimage.transform import resize
+from PIL import Image
 
 import predict
 import utils
 
-# Specify canvas parameters in application
+# specify canvas parameters in application
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("freedraw", "line", "rect", "circle")
 )
@@ -17,39 +17,41 @@ stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 
 realtime_update = True
 
-st.header('Draw here and I will predict!')
+st.header("Draw here and I will predict!")
 
 # create a canvas component
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)", 
+    fill_color="rgba(255, 165, 0, 0.3)",
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     update_streamlit=realtime_update,
     height=500,
-    width=500, 
+    width=500,
     drawing_mode=drawing_mode,
     key="canvas",
 )
 
 # predict here
-img_cmyk= canvas_result.image_data
+img_cmyk = canvas_result.image_data
 if img_cmyk is not None:
-
-    # convert to grayscale
-    img_gray = np.sum(img_cmyk, axis=-1)
-    # rescale the pixel values to be in the range [0, 255]
-    img_gray = utils.rescale_array(img_gray, (0, 255))
-    # resize the image to 28x28
-    img_gray = resize(img_gray, (28, 28))
-    # flatten
-    img_gray = img_gray.flatten().reshape(1, -1)
-
-    # predict
-    try:
-        y_pred = predict.predict(x=img_gray, with_preprocesing= True)
-        result = f'This is :red[{y_pred[0]}]!'
-        st.header(result)
-    except ValueError:
-        pass
-
+    pass
+    # TODO: convert RGBA to grayscale
+    # convert rgba to grayscale 
+    # rescale
+    # then convert back to array
+    # img_gray = Image.fromarray(img_cmyk, "RGBA").convert('LA')
+    # img_gray = resize(np.asarray(img_gray), output_shape= (28, 28))
     
+    # # flatten
+    # img_gray = img_gray.flatten().reshape(1, -1)
+
+    # st.write(img_gray.shape)
+
+
+    # # predict
+    # try:
+    #     y_pred = predict.predict(x=img_gray, with_preprocesing=True)
+    #     result = f"This is :red[{y_pred[0]}]!"
+    #     st.header(result)
+    # except ValueError:
+    #     pass
