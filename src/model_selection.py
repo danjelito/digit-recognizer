98f9028f6167dc115043fcb970d6e8d722af5111
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-10 19:44:38 +0700

    Change df append to df concat.

--- src/model_selection.py
+++ src/model_selection.py
@@ -17,10 +17,9 @@ import config
 import utils
 
 
-def main(augment= False):
-
+def main(augment=False):
     # load dataset
-    x_train, y_train = utils.load_dataset(load_test_set= False)
+    x_train, y_train = utils.load_dataset(load_test_set=False)
 
     if augment:
         # augment dataset
@@ -42,17 +41,25 @@ def main(augment= False):
 
     # cross val the models, append result to result df
     df_result = pd.DataFrame(
-        columns=["fold", "model", "accuracy", "f1", "precision", "recall", "prediction_time"]
+        columns=[
+            "fold",
+            "model",
+            "accuracy",
+            "f1",
+            "precision",
+            "recall",
+            "prediction_time",
+        ]
     )
 
     for key, classifier in classifiers.items():
-        
         model_name = classifier.__class__.__name__
 
         # run kfold
-        kfold= StratifiedKFold(n_splits= 3, shuffle= True, random_state= config.RANDOM_STATE)
+        kfold = StratifiedKFold(
+            n_splits=3, shuffle=True, random_state=config.RANDOM_STATE
+        )
         for fold, (train_idx, val_idx) in enumerate(kfold.split(x_train, y_train)):
-            
             # set train val set
             x_t, y_t = x_train[train_idx], y_train[train_idx]
             x_v, y_v = x_train[val_idx], y_train[val_idx]
@@ -67,30 +74,38 @@ def main(augment= False):
 
             # get score and append
             acc, f1, prec, rec = utils.get_score(y_v, y_pred)
-            df_result = df_result.append(
-                {   
-                    "fold": fold,
-                    "model": model_name,
-                    "accuracy": acc,
-                    "f1": f1,
-                    "precision": prec,
-                    "recall": rec,
-                    "prediction_time": elapsed_time,
-                },
+            df_result = pd.concat(
+                [
+                    df_result,
+                    pd.DataFrame(
+                        {
+                            "fold": fold,
+                            "model": model_name,
+                            "accuracy": acc,
+                            "f1": f1,
+                            "precision": prec,
+                            "recall": rec,
+                            "prediction_time": elapsed_time,
+                        },
+                        index=[0],
+                    ),
+                ],
                 ignore_index=True,
             )
 
     # save result
-    df_result= (df_result
-        .groupby('model')
-        .agg({
-            'accuracy': 'mean',
-            'f1': 'mean',
-            'precision': 'mean',
-            'recall': 'mean',
-            'prediction_time': 'sum',
-        })
-        .sort_values('f1', ascending= False)
+    df_result = (
+        df_result.groupby("model")
+        .agg(
+            {
+                "accuracy": "mean",
+                "f1": "mean",
+                "precision": "mean",
+                "recall": "mean",
+                "prediction_time": "sum",
+            }
+        )
+        .sort_values("f1", ascending=False)
     )
     # df_result.to_csv(config.PATH_MODEL_SELECTION_RES, index=False)
     print(df_result)
@@ -99,8 +114,8 @@ def main(augment= False):
 if __name__ == "__main__":
     # if not already run
     if not Path(config.PATH_MODEL_SELECTION_RES).exists():
-        main(augment= False)
+        main(augment=False)
     else:
-        df_result= pd.read_csv(config.PATH_MODEL_SELECTION_RES)
-        df_result= df_result.sort_values('f1', ascending= False)
+        df_result = pd.read_csv(config.PATH_MODEL_SELECTION_RES)
+        df_result = df_result.sort_values("f1", ascending=False)
         print(df_result)
