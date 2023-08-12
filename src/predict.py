8f99b07e1922a5c41e964873f97f859f9774c23a
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-12 19:51:49 +0700

    Add model selection in predict function.

--- src/predict.py
+++ src/predict.py
@@ -1,18 +1,29 @@
 import config
 import pickle
-import model
 
-# load model and pipeline
-with open(config.PATH_MODEL, "rb") as file:
-    model = pickle.load(file)
-with open(config.PATH_PIPELINE, "rb") as file:
-    pipeline = pickle.load(file)
 
 # predict
-def predict(x, with_preprocesing= True):
+def predict(model, x, with_preprocesing=True):
     
+    if model == "Random Forest":
+        path_model = config.PATH_MODEL_RF
+    elif model == "K-Nearest Classifier":
+        path_model = config.PATH_MODEL_KNC
+    elif model == "Logistic Regression":
+        path_model = config.PATH_MODEL_LR
+    else:
+        raise ValueError("Model unrecognized.")
+
+    # load model and pipeline
+    with open(path_model, "rb") as file:
+        model = pickle.load(file)
+    with open(config.PATH_PIPELINE, "rb") as file:
+        pipeline = pickle.load(file)
+
+    # preprocess
     if with_preprocesing:
         x = pipeline.transform(x)
         return model.predict(x)
-    
+
+    # predict
     return model.predict(x)
