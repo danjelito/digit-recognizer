4f413d4038ad4f1b3204a8b7bce9bb890ae67ba7
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-12 13:01:57 +0700

    Finish hp tuning.

--- src/model.py
+++ src/model.py
@@ -7,16 +7,30 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import config
 
-pipeline = Pipeline([
-    ("scale", StandardScaler()),
-    ("compress", PCA(n_components=0.95, random_state=config.RANDOM_STATE)),
-    ("select_k_best", SelectKBest(k=100, score_func=mutual_info_classif)),
-])
+pipeline = Pipeline(
+    [
+        ("scale", StandardScaler()),
+        ("compress", PCA(n_components=0.95, random_state=config.RANDOM_STATE)),
+        ("select_k_best", SelectKBest(k=100, score_func=mutual_info_classif)),
+    ]
+)
 
-knc_param = {'n_neighbors': 5, 'weights': 'distance', 'algorithm': 'auto', 'leaf_size': 35}
-# lr_param = 
-# rf_param = 
+knc_param = {
+    "n_neighbors": 4,
+    "weights": "distance",
+    "algorithm": "ball_tree",
+    "leaf_size": 12,
+}
+lr_param = {"C": 2.2458127933062992, "solver": "newton-cg"}
+rf_param = {
+    "n_estimators": 109,
+    "criterion": "gini",
+    "max_depth": 60,
+    "min_sample_split": 8,
+    "min_sample_leaf": 1,
+    "max_features": "sqrt",
+}
 
 knc = KNeighborsClassifier(**knc_param)
-# rf = RandomForestClassifier(**rf_param)
-# lr = LogisticRegression(**lr_param)
+rf = RandomForestClassifier(**rf_param)
+lr = LogisticRegression(**lr_param)
