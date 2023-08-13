fcc0c13c7360f8d1101eaf30d69c3dca0b6b98dd
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-13 09:54:51 +0700

    Add spinner.

--- src/streamlit.py
+++ src/streamlit.py
@@ -71,7 +71,8 @@ if img_rgba is not None:
 # predict if there is image
 if np.sum(img_rgba) != 0:
     try:
-        y_pred = predict.predict(model=model, x=img_flat, with_preprocesing=True)
+        with st.spinner('Wait for it, some models are slower than others...'):
+            y_pred = predict.predict(model=model, x=img_flat, with_preprocesing=True)
         result = f"This is :red[{y_pred[0]}]!"
         st.header(result)
     except ValueError:
